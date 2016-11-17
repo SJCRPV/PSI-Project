851ce54ad183c1681e56b4a10b04ce279cb3a425
SJCRPV <sjcrpv.games@hotmail.com>
2016-11-17 19:25:35 +0000

    NavBar

---
+++ Marriage Bureau/Assets/Scripts/ButtonPress.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonPress : MonoBehaviour {
+    [SerializeField]
+    private int sceneToLoadIndex;
+
+    public void loadScene()
+    {
+        SceneManager.LoadScene(sceneToLoadIndex);
+    }
+}
