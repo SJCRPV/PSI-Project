0a28473a2efec5525475e404580cc724ec578a7b
sebastiao jose da costa reis perestrello de vasconcellos (r2014525) <r2014525@isegi.unl.pt>
2016-11-22 13:50:49 +0000

    Progress Understood how the scroll content works Scroll is working fine (lacking mousewheel) Images are in the right place Back to top button is working (needs to make the transition smooth)

---
+++ Marriage Bureau/Assets/Scripts/ScrollToTop.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScrollToTop : MonoBehaviour {
+
+    private Scrollbar scrollbarScript;
+
+    public void moveToTop()
+    {
+        scrollbarScript.value = 1;
+    }
+
+	// Use this for initialization
+	void Start () {
+        scrollbarScript = GameObject.Find("Scrollbar").GetComponent<Scrollbar>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
