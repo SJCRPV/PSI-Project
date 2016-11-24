4ed9f2c2fb0cc95d48dae0213452e39b60046022
sebastiao jose da costa reis perestrello de vasconcellos (r2014525) <r2014525@isegi.unl.pt>
2016-11-24 15:18:02 +0000

    Rethinking Deleted the login page and the register will probably follow. It's cleaner. Note: This is being done with a 800x600 resolution reference. It may cause problems with in higher ones.

---
+++ Marriage Bureau/Assets/Scripts/Utility/AdaptToScreenSize.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdaptToScreenSize : MonoBehaviour {
+
+    private RectTransform canvasTransform;
+    private RectTransform objectTransform;
+
+    private void adaptToScreenSize()
+    {
+        //Debug.Log("Canvas width: " + canvasTransform.rect.width + "\nCanvas height: " + canvasTransform.rect.height);
+        objectTransform.sizeDelta = new Vector2(canvasTransform.rect.width, canvasTransform.rect.height);
+    }
+
+	// Use this for initialization
+	void Start () {
+        canvasTransform = gameObject.transform.parent.GetComponentInParent<RectTransform>();
+        objectTransform = gameObject.GetComponent<RectTransform>();
+        adaptToScreenSize();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Marriage Bureau/Assets/Scripts/Utility/SwapContentOnClick.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SwapContentOnClick : MonoBehaviour {
+
+    [SerializeField]
+    private Sprite idleSprite;
+    [SerializeField]
+    private Sprite clickedSprite;
+    private Text contentTextScript;
+    private Text buttonTextScript;
+    private Image imageScript;
+
+    private void swapText()
+    {
+        contentTextScript.text = imageScript.gameObject.transform.GetChild(0).GetComponent<Text>().text;
+    }
+
+    public void swapSprite()
+    {
+        if(imageScript.sprite != clickedSprite)
+        {
+            imageScript.sprite = clickedSprite;
+            buttonTextScript.color = Color.white;
+            swapText();
+        }
+    }
+
+    public void resetSprite()
+    {
+        imageScript.sprite = idleSprite;
+        buttonTextScript.color = Color.black;
+    }
+
+	// Use this for initialization
+	void Start () {
+        imageScript = this.gameObject.GetComponent<Image>();
+        buttonTextScript = this.gameObject.GetComponentInChildren<Text>();
+        if(buttonTextScript == null)
+        {
+            Debug.LogError("buttonTextScript is empty");
+        }
+        contentTextScript = GameObject.Find("Content").gameObject.GetComponentInChildren<Text>();
+	}
+}
